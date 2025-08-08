e0152f9b08f57cbb46a5aeca7d8db50ac12bdaad
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-08 23:38:17 +0400

    // Get all rooms

--- Repository/IRoomRepository.cs
+++ Repository/IRoomRepository.cs
@@ -5,7 +5,7 @@ namespace SimpleHotelRoomManagementProjectORM.Repository
     //// Interface to define the contract for Room repository operations
     public interface IRoomRepository
     {
-        void AddRoom(Room room);
+        void AddRoom(Room room); // Get all rooms
         void DeleteRoom(int id);
         List<Room> GetAllRooms();
         Room GetRoomById(int id);
