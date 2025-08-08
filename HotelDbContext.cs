7bc8c1bdbe6f97179677471dabe90ea74a29738f
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-08 20:53:22 +0400

    Represents the Rooms table in the database

--- HotelDbContext.cs
+++ HotelDbContext.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimpleHotelRoomManagementProjectORM.Models;
 
 namespace SimpleHotelRoomManagementProjectORM
 {
-    public class HotelDbContext
+    public class HotelDbContext : DbContext
     {
+        public DbSet<Room> Rooms { get; set; } // Represents the Rooms table in the database
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
     }
 }
