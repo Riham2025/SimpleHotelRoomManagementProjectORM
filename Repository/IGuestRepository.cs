18a616e51b98170a541d07ecdeaed368eeaf3b4a
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-08 23:55:30 +0400

    Get a guest by email

--- Repository/IGuestRepository.cs
+++ Repository/IGuestRepository.cs
@@ -8,7 +8,7 @@ namespace SimpleHotelRoomManagementProjectORM.Repository
         void AddGuest(Guest guest); // Add a new guest
         void DeleteGuest(int id); // Delete a guest by ID
         List<Guest> GetAllGuests(); // Get all guests
-        Guest GetGuestByEmail(string email);
+        Guest GetGuestByEmail(string email); // Get a guest by email
         Guest GetGuestById(int id);
         void UpdateGuest(Guest guest);
     }
