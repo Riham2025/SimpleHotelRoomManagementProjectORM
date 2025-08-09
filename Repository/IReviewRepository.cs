f5d8bdee0c98212dcfd88528830437bd20367d3c
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-09 10:22:27 +0400

    Delete a review by its ID

--- Repository/IReviewRepository.cs
+++ Repository/IReviewRepository.cs
@@ -5,7 +5,7 @@ namespace SimpleHotelRoomManagementProjectORM.Repository
     public interface IReviewRepository // Interface for Review repository
     {
         void AddReview(Review review); // Add a new review to the database
-        void DeleteReview(int id);
+        void DeleteReview(int id); // Delete a review by its ID
         List<Review> GetAllReviews();
         double GetAverageRatingForGuest(int guestId);
         Review GetReviewById(int id);
