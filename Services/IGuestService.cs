1e2f6e9376a18b7ad2d590c2ec2975a33f46d032
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-09 19:36:35 +0400

    Delete a guest by ID (may add business rules later, e.g., cannot delete with active bookings)

--- Services/IGuestService.cs
+++ Services/IGuestService.cs
@@ -2,9 +2,9 @@
 
 namespace SimpleHotelRoomManagementProjectORM.Services
 {
-    public interface IGuestService
+    public interface IGuestService 
     {
-        bool DeleteGuest(int id, out string error);
+        bool DeleteGuest(int id, out string error); // Delete a guest by ID (may add business rules later, e.g., cannot delete with active bookings)
         List<Guest> GetAllGuests();
         Guest GetGuestByEmail(string email);
         Guest GetGuestById(int id);
