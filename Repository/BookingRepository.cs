2372952e10f3f6ddd272bdbc3a21a2f25ab3db5f
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-09 09:33:46 +0400

    Load the associated Room

--- Repository/BookingRepository.cs
+++ Repository/BookingRepository.cs
@@ -45,7 +45,7 @@ namespace SimpleHotelRoomManagementProjectORM.Repository
         {
             // Return bookings where GuestId matches, including Room info
             return _context.Bookings
-                           .Include(b => b.Room)
+                           .Include(b => b.Room) // Load the associated Room
                            .Where(b => b.GuestId == guestId)
                            .ToList();
         }
