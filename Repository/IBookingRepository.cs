033f1f8f5696d9d18bc539eb7a6edccd092a5cd5
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-09 09:59:07 +0400

    Retrieve active bookings for a specific room, optionally on a specific date

--- Repository/IBookingRepository.cs
+++ Repository/IBookingRepository.cs
@@ -6,7 +6,7 @@ namespace SimpleHotelRoomManagementProjectORM.Repository
     {
         void AddBooking(Booking booking); // Add a new booking to the database
         void DeleteBooking(int id); // Delete a booking by its ID
-        List<Booking> GetActiveBookingsForRoom(int roomId, DateTime? onDate = null);
+        List<Booking> GetActiveBookingsForRoom(int roomId, DateTime? onDate = null); // Retrieve active bookings for a specific room, optionally on a specific date
         List<Booking> GetAllBookings();
         Booking GetBookingById(int id);
         List<Booking> GetBookingsByGuest(int guestId);
