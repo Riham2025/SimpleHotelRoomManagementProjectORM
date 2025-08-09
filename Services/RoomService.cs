b900d9f1debea0a034a927f21c12b5b9c6edb71d
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-09 19:45:32 +0400

    Reference to the data-access layer (Room repository)

--- Services/RoomService.cs
+++ Services/RoomService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleHotelRoomManagementProjectORM.Repository;
 
 namespace SimpleHotelRoomManagementProjectORM.Services
 {
     public class RoomService
     {
+        // Reference to the data-access layer (Room repository)
+        private readonly IRoomRepository _roomRepo;
     }
 }
