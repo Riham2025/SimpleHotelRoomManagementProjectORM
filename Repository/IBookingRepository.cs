80f10fe8b1159ceaaa7693001e9b69824ca7be9c
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-09 09:58:28 +0400

    Add a new booking to the database

--- Repository/IBookingRepository.cs
+++ Repository/IBookingRepository.cs
@@ -4,7 +4,7 @@ namespace SimpleHotelRoomManagementProjectORM.Repository
 {
     public interface IBookingRepository // Interface for Booking repository
     {
-        void AddBooking(Booking booking);
+        void AddBooking(Booking booking); // Add a new booking to the database
         void DeleteBooking(int id);
         List<Booking> GetActiveBookingsForRoom(int roomId, DateTime? onDate = null);
         List<Booking> GetAllBookings();
