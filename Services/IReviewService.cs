9fe24e1c55dcb91e20fceb4d69a46d56f13cdfcb
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-10 12:19:25 +0400

    Create a new review with validations

--- Services/IReviewService.cs
+++ Services/IReviewService.cs
@@ -4,7 +4,7 @@ namespace SimpleHotelRoomManagementProjectORM.Services
 {
     public interface IReviewService // High-level contract for Review operations
     {
-        bool AddReview(int guestId, int rating, string? comment, out string error);
+        bool AddReview(int guestId, int rating, string? comment, out string error); // Create a new review with validations
         bool DeleteReview(int reviewId, out string error);
         List<Review> GetAllReviews();
         double GetAverageRatingForGuest(int guestId);
