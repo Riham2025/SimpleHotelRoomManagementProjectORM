7fb81fb4c02db4704157fbe30556eb6c08d36b1b
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-10 09:52:04 +0400

    void UpdateBookingDates(int bookingId, DateTime newCheckIn, DateTime newCheckOut); // Update booking dates

--- Services/IBookingService.cs
+++ Services/IBookingService.cs
@@ -8,6 +8,6 @@ namespace SimpleHotelRoomManagementProjectORM.Services
         void CreateBooking(int guestId, int roomId, DateTime checkIn, DateTime checkOut); // Create a new booking
         List<Booking> GetAllBookings(); // Get all bookings
         Booking GetBookingById(int bookingId);// Get a booking by ID
-        void UpdateBookingDates(int bookingId, DateTime newCheckIn, DateTime newCheckOut);
+        void UpdateBookingDates(int bookingId, DateTime newCheckIn, DateTime newCheckOut); // Update booking dates
     }
 }
