6712f49ca48ea741f1b6fbd1d242bc31e4073f6a
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-10 13:48:15 +0400

    Guest repository

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace SimpleHotelRoomManagementProjectORM
             HotelDbContext dbContext = new HotelDbContext();// Create the database context
 
             RoomRepository roomRepo = new RoomRepository(dbContext); // Room repository
+            GuestRepository guestRepo = new GuestRepository(dbContext);// Guest repository
+            BookingRepository bookingRepo = new BookingRepository(dbContext);
+            ReviewRepository reviewRepo = new ReviewRepository(dbContext);    
+
 
 
         }
