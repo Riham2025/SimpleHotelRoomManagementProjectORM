3fab238f2c651a5dd3157806661d740bbd315cf3
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-10 09:51:00 +0400

    void CancelBooking(int bookingId); // Cancel a booking by ID

--- Services/IBookingService.cs
+++ Services/IBookingService.cs
@@ -2,9 +2,9 @@
 
 namespace SimpleHotelRoomManagementProjectORM.Services
 {
-    public interface IBookingService
+    public interface IBookingService // Interface for booking operations
     {
-        void CancelBooking(int bookingId);
+        void CancelBooking(int bookingId); // Cancel a booking by ID
         void CreateBooking(int guestId, int roomId, DateTime checkIn, DateTime checkOut);
         List<Booking> GetAllBookings();
         Booking GetBookingById(int bookingId);
