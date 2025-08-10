b04dab967028fb472efda9feb080a303e37d7ebc
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-10 09:06:34 +0400

    To interact with booking data storage

--- Services/BookingService.cs
+++ Services/BookingService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleHotelRoomManagementProjectORM.Repository;
 
 namespace SimpleHotelRoomManagementProjectORM.Services
 {
     public class BookingService
     {
+
+        private readonly IBookingRepository _bookingRepository;  // To interact with booking data storage
     }
 }
